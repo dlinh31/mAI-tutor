9b2a29aa325f2c250388e8d11cea68fc53d48e87
Liam Ha <haduylinh2005.work@gmail.com>
2024-04-12 18:30:13 -0400

    add create ai chatroom

--- server/models/chatSessionModel.js
+++ server/models/chatSessionModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const chatSessionSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    chatName: {type: String, required: true},
     createdAt: { type: Date, default: Date.now },
     messages: [
         {
